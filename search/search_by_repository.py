fd4d9c62a9ad7f8db3827a2b3d48b6723e9cc36f
Ryu-GeonWoo <john2318@naver.com>
2023-12-27 01:03:46 +0900

    update search_by_repository 검색조건에 따른 모든 repo 검색후 oage 별로 txt로 저장

--- search/search_by_repository.py
+++ search/search_by_repository.py
@@ -7,22 +7,34 @@ from search.search_option import *
 def search_by_repository(headers):
     from search.search import log_txt
 
-    save_repository_info("Search type : Repository\n", log_txt)
+    save_repository_info("Search type: Repository\n", log_txt)
     option = search_option()
 
     # GitHub 검색 API를 사용하여 리포지토리 검색
     search_url = 'https://api.github.com/search/repositories'
     query = f'language:{option.language_input} stars:>{option.min_stars} forks:>{option.min_forks} pushed:>{option.updated_after}'
-    params = {'q': query, 'sort': 'stars', 'order': 'desc', 'per_page': option.per_page}  # 검색할 페이지 수만큼 가져옴
+    params = {'q': query, 'sort': 'stars', 'order': 'desc', 'per_page': option.per_page, 'page': 1}
 
     response = requests.get(search_url, headers=headers, params=params)
 
     # 응답 상태 및 내용 확인
     if response.status_code == 200:
         data = response.json()
-        # json txt로 저장
-        save_json_to_txt(data, "repository")
-        github_crawler(data)
+        total_count = data['total_count']
+        total_page = int(total_count / option.per_page) + 1
+
+        for i in range(1, total_page + 1):
+            params = {'q': query, 'sort': 'stars', 'order': 'desc', 'per_page': option.per_page, 'page': i}
+            response = requests.get(search_url, headers=headers, params=params)
+
+            if response.status_code == 200:
+                page_data = response.json()
+                save_json_to_txt(page_data, f"repository{i}")
+            else:
+                status_error = f"Unable to retrieve data for page {i}. Status code: {response.status_code}"
+                print(status_error)
+                save_repository_info(status_error, log_txt)
+                print(response.text)
     else:
         status_error = f"Unable to retrieve data. Status code: {response.status_code}"
         print(status_error)
--- search/search_option.py
+++ search/search_option.py
@@ -2,8 +2,8 @@ from datetime import datetime, timedelta
 
 class search_option():
     language_input = 'python'
-    min_stars = 10
-    min_forks = 10
+    min_stars = 10000
+    min_forks = 1000
     updated_after = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d')
     per_page = 100 # 최대값 100
     page = 1
